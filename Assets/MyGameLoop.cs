ef8e0a0c56435070b4fe686a73b30728a89bc944
CEOFYEAST <89040723+CEOFYEAST@users.noreply.github.com>
2023-04-26 18:20:23 -0600

    more gameloop work and timer revamp

--- Assets/MyGameLoop.cs
+++ Assets/MyGameLoop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,37 +8,97 @@ using TMPro;
 
 public class MyGameLoop : MonoBehaviour
 {
+    #region Private Serializable Fields
+
+    /// text objects used to display game and start timers
     [SerializeField]
     private TMPro.TextMeshProUGUI popupText;
 
     [SerializeField]
     private TMPro.TextMeshProUGUI timerText;
 
-    // variables required for starting timer
-    private int startingTimerLength = 15;
-    private int startingTimerCurrentInterval;
+    /// trigger that tracks goals
+    [SerializeField]
+    private GameObject scoreTrigger;
 
-    // variables required for game timer
-    private int timerLength = 60;
-    private int timerCurrentInterval;
+    #endregion
+
+
+    #region Unity Monobehaviour Callbacks
 
     // Start is called before the first frame update
     void Start()
     {
-        // sets current intervals
-        startingTimerCurrentInterval = startingTimerLength;
-        timerCurrentInterval = timerLength;
-
-        // starts starting timer
-        gameObject.GetComponent<Timers>().StartTimer(startingTimerLength);
+        StartGameLoop();
     }
 
-    public void UpdatePopupText(){
-        popupText.text = "Starting In " + startingTimerCurrentInterval;
-        startingTimerCurrentInterval--;
+    #endregion
+
+
+    #region Private Methods
+
+    private void StartGameLoop(){
+        // prevents players from scoring during the warmup
+        scoreTrigger.SetActive(false);
+
+        // starts the timer
+        StartCoroutine(Timer(popupText, "Warmup ending in ", 15, BlockOne));
     }
 
-    public void StartGameLoop(){
-        popupText.transform.parent.gameObject.SetActive(false);
+        /// <summary>
+        /// methods that constitute the game loop, which 
+        ///  - gameloop is broken up into blocks that can be called individually, allows for timers
+        /// <summary>
+        #region Game Loop Blocks
+
+        private void BlockOne(){
+            // disables popup text for the time being
+            popupText.transform.parent.gameObject.SetActive(false);
+
+            // allows players to score 
+            scoreTrigger.SetActive(true);
+
+            // starts game timer
+            StartCoroutine(Timer(timerText, "", 60, BlockTwo));
+        }
+
+        private void BlockTwo(){
+
+        }
+
+        #endregion
+
+    #endregion
+
+
+    #region Private Inumerators
+
+    /// <summary>
+    /// timer used during game start and during the game that  
+    /// - counts down from a given length (length)
+    /// - updates a given text object (textToUpdate) with the current time displayed after a given string (message)
+    ///     - message gives context to the timer's purpose
+    /// - calls a given method (callback) upon reaching zero
+    /// <summary>
+    private IEnumerator Timer(TMPro.TextMeshProUGUI textToUpdate, string message, int length, Action callback){
+        length--;
+
+        while (true)
+        {
+            //updates every tenth second 
+            for (int i = 9; i >= 0; i--)
+            {
+                yield return new WaitForSeconds(.1f);
+                textToUpdate.text = message + length + ":" + i;
+            }
+            if (length == 0)
+            {
+                callback?.Invoke();
+                break;
+            }
+            length--;
+        }
     }
+
+    #endregion
 }
